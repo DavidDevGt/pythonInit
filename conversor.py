4f54ee2f5225fedada5520d6ef54fe75c580f9a4
Davidvl <josuedavidvl18@gmail.com>
2023-04-16 01:01:44 -0600

    Conversor de monedas, ahora con lista desplegable para su facilidad de uso

--- conversor.py
+++ conversor.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import requests
 
 def convertir_moneda(origen, destino, cantidad):
@@ -10,24 +11,25 @@ def convertir_moneda(origen, destino, cantidad):
     return resultado
 
 def convertir():
-    moneda_origen = origen_entry.get()
-    moneda_destino = destino_entry.get()
+    moneda_origen = origen_combo.get()
+    moneda_destino = destino_combo.get()
     cantidad = float(cantidad_entry.get())
     resultado = convertir_moneda(moneda_origen, moneda_destino, cantidad)
     resultado_label.config(text=f"{cantidad} {moneda_origen} es equivalente a {resultado} {moneda_destino}")
 
 root = tk.Tk()
-root.title("Conversor de monedas")
+root.title("Conversor de monedas v2")
 
 origen_label = tk.Label(root, text="Moneda de origen:")
 origen_label.grid(row=0, column=0)
-origen_entry = tk.Entry(root)
-origen_entry.grid(row=0, column=1)
+monedas = ["USD", "EUR", "JPY", "GBP", "CAD", "CHF", "AUD", "CNY", "HKD", "NZD", "SEK", "KRW", "SGD", "NOK", "MXN", "INR", "RUB", "ZAR", "TRY", "BRL", "TWD", "DKK", "PLN", "THB", "IDR", "HUF", "CZK", "ILS", "CLP", "PHP"]
+origen_combo = ttk.Combobox(root, values=monedas)
+origen_combo.grid(row=0, column=1)
 
 destino_label = tk.Label(root, text="Moneda de destino:")
 destino_label.grid(row=1, column=0)
-destino_entry = tk.Entry(root)
-destino_entry.grid(row=1, column=1)
+destino_combo = ttk.Combobox(root, values=monedas)
+destino_combo.grid(row=1, column=1)
 
 cantidad_label = tk.Label(root, text="Cantidad:")
 cantidad_label.grid(row=2, column=0)
